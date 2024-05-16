fae71c8b4191382e8c04627ef722d2896139047d
valentino <valecasesi@gmail.com>
2024-05-16 19:15:55 -0300

    changes in new OS

--- src/app/components/OS/create-os/create-os.component.ts
+++ src/app/components/OS/create-os/create-os.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, NgForm, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ToastrService} from "ngx-toastr";
@@ -14,66 +14,53 @@ import {__importDefault} from "tslib";
 export class CreateOSComponent implements OnInit {
   OSForm: FormGroup;
 
-  token: string | null = null;
-  master: string | null = null;
-
-  titulo = 'Crete OS';
+  titulo= 'Crete OS';
   _id: String | null;
-
   constructor(private fb: FormBuilder,
               private router: Router,
               private toastr: ToastrService,
               private _osService: ObraSocialService,
               private aRouter: ActivatedRoute) {
-    this.OSForm = this.fb.group({
-      nombre: ['', Validators.required]
-    })
-    this._id = this.aRouter.snapshot.paramMap.get('id');
+  this.OSForm=this.fb.group({
+    nombre: ['', Validators.required],
+    imgURL: ['', Validators.required]
+
+  })
+    this._id=this.aRouter.snapshot.paramMap.get('id');
   }
 
-  /* const os: ObraSocial={
-     nombre: this.OSForm.get('nombre')?.value,
-   };*/
+ /* const os: ObraSocial={
+    nombre: this.OSForm.get('nombre')?.value,
+  };*/
 
   ngOnInit(): void {
-    if (localStorage.getItem('token') != null) {
-      this.token = localStorage.getItem('token');
-    } else {
-      this.token = null;
-    }
-    if (localStorage.getItem('master') != null) {
-      this.master = localStorage.getItem('master');
-      ;
-    } else {
-      this.master = null;
-    }
     this.isEdit();
   }
 
 
-  createOS(OSForm: NgForm) {
-    const os: ObraSocial = {
-      nombre: this.OSForm.get('nombre')?.value,
-      imgURL: this.OSForm.get('imgURL')?.value
+  createOS(OSForm: NgForm){
+     const os: ObraSocial={
+        nombre: this.OSForm.get('nombre')?.value,
+        imgURL : this.OSForm.get('imgURL')?.value
     };
     console.log(os)
     this._osService.createOS(os).subscribe(
       res => {
         console.log(res);
-        this.toastr.success('The OS has been successfully created!', 'OS added!')
+    this.toastr.success('The OS has been successfully created!', 'OS added!')
         this.router.navigate(['/list-os'])
-      }, error => {
+    }, error => {
         console.log(error);
         this.OSForm.reset();
       })
   }
 
-  isEdit() {
-    if (this._id !== null) {
-      this.titulo = 'Edit OS';
+  isEdit(){
+    if (this._id!==null){
+      this.titulo= 'Edit OS';
       this._osService.getOS(this._id).subscribe(
-        data => {
-          this.OSForm.patchValue({
+        data=>{
+          this.OSForm.setValue({
               nombre: data.nombre,
               imgURL: data.imgURL
             }
@@ -88,7 +75,7 @@ export class CreateOSComponent implements OnInit {
       nombre: this.OSForm.get('nombre')?.value,
       imgURL: this.OSForm.get('imgURL')?.value
     }
-    this._osService.updateOS(id, os).subscribe(res => {
+    this._osService.updateOS(id, os).subscribe( res=> {
       this.toastr.success('The OS was successfully updated', 'OS Updated!')
       this.router.navigate(['/list-os'])
     }, error => {
